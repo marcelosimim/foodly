4708c0ac40a120854c960fdba49514f1056484ac
Marcelo <marcelosimim@gmail.com>
2022-11-01 14:10:10 -0300

    first commit

---
+++ Foodly/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Foodly
+//
+//  Created by Marcelo Simim Santos on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
